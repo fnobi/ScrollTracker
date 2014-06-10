044f69a481573169a2f6b8fa2e2e4caace2bb00c
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-06-10 21:09:13 +0900

    README更新。

--- src/js/ScrollTracker_demo.js
+++ src/js/ScrollTracker_demo.js
@@ -1,1 +1,26 @@
-new ScrollTracker();
+// init ScrollTracker
+var scrollTracker = new ScrollTracker({
+    interval: 500 // throttle interval
+});
+
+/* ==================================== *
+ * catch scroll event
+ * ==================================== */
+scrollTracker.on('scroll', function (e) {
+    console.log('scroll top: %s', e.scrollTop);
+});
+
+/* ==================================== *
+ * catch section change event
+ * ==================================== */
+var $sections = $('section');
+
+scrollTracker.on('changeSection', function (e) {
+    // run on changing section in window
+    var index = e.index;
+    console.log('current section: %s', $sections.get(index).id);
+});
+
+// set sections
+scrollTracker.setSections($sections);
+
