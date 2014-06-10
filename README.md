25386094ecebb6b0488960f997bd2e5b27e7dd56
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-06-10 20:36:38 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# ScrollTracker
+===============
+
+Manage scroll events and sections.
+
+## install
+
+```
+bower install ScrollTracker
+```
+
+## usage
+
+```javascript
+new ScrollTracker();
+
+```
---
+++ src/js/ScrollTracker.js
@@ -0,0 +1,61 @@
+var ScrollTracker = function (opts) {
+    opts = opts || {};
+    this.interval = opts.interval || 0;
+
+    this.sectionIndex = null;
+    this.sections = null;
+    this.$window = $(window);
+
+    this.startTracking();
+};
+inherits(ScrollTracker, EventEmitter);
+
+ScrollTracker.prototype.startTracking = function () {
+    var self = this;
+    var $window = this.$window;
+    $window.on('scroll', throttle(function () {
+        self.handleScroll();
+    }, this.interval));
+};
+
+ScrollTracker.prototype.scrollTop = function () {
+    return this.$window.scrollTop();
+};
+
+ScrollTracker.prototype.setSections = function (sections) {
+    this.sections = sections;
+    this.checkSection(this.scrollTop());
+};
+
+ScrollTracker.prototype.handleScroll = function () {
+    var scrollTop = this.scrollTop();
+    this.emit('scroll', {
+        scrollTop: scrollTop
+    });
+    this.checkSection(scrollTop);
+};
+
+ScrollTracker.prototype.checkSection = function (scrollTop) {
+    if (!this.sections) {
+        return;
+    }
+
+    if (isNaN(scrollTop)) {
+        scrollTop = this.scrollTop();
+    }
+
+    var index = 0;
+
+    this.sections.each(function (i, el) {
+        if (scrollTop > el.offsetTop) {
+            index = i;
+        }
+    });
+
+    if (this.sectionIndex !== index) {
+        this.sectionIndex = index;
+        this.emit('changeSection', {
+            index: index
+        });
+    }
+};
