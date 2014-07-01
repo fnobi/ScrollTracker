7429cbad5a43649956c187a79c9d88b9f562123e
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-07-02 03:18:30 +0900

    smoothScroll (jumpTo・jumpToSection)

--- ScrollTracker.js
+++ ScrollTracker.js
@@ -9941,7 +9941,7 @@ var ScrollTracker = function (opts) {
     this.interval = opts.interval || 0;
 
     this.sectionIndex = null;
-    this.sections = null;
+    this.$section = null;
     this.$window = $(window);
 
     this.startTracking();
@@ -9960,9 +9960,9 @@ ScrollTracker.prototype.scrollTop = function () {
     return this.$window.scrollTop();
 };
 
-ScrollTracker.prototype.setSections = function (sections) {
-    this.sections = sections;
-    this.checkSection(this.scrollTop());
+ScrollTracker.prototype.setSections = function ($section) {
+    this.$section = $section;
+    this.checkSection();
 };
 
 ScrollTracker.prototype.handleScroll = function () {
@@ -9973,30 +9973,106 @@ ScrollTracker.prototype.handleScroll = function () {
     this.checkSection(scrollTop);
 };
 
-ScrollTracker.prototype.checkSection = function (scrollTop) {
-    if (!this.sections) {
-        return;
+ScrollTracker.prototype.getSection = function (scrollTop) {
+    var $section = this.$section;
+
+    if (!$section) {
+        return null;
     }
 
     if (isNaN(scrollTop)) {
         scrollTop = this.scrollTop();
     }
 
-    var index = 0;
+    var index = -1;
     var sectionId;
 
-    this.sections.each(function (i, el) {
+    $section.each(function (i, el) {
         if (scrollTop >= el.offsetTop) {
             index = i;
             sectionId = el.id;
         }
     });
 
-    if (this.sectionIndex !== index) {
-        this.sectionIndex = index;
-        this.emit('changeSection', {
-            index: index,
-            sectionId: sectionId
-        });
+    return {
+        index: index,
+        sectionId: sectionId
+    };
+};
+
+ScrollTracker.prototype.checkSection = function (scrollTop) {
+    var section = this.getSection(scrollTop);
+
+    if (!section) {
+        return;
     }
+
+    if (this.sectionIndex !== section.index) {
+        this.sectionIndex = section.index;
+        this.emit('changeSection', section);
+    }
+};
+
+ScrollTracker.prototype.jumpTo = function (to, duration, easing) {
+    to = isNaN(to) ? 0 : to;
+    duration = isNaN(duration) ? 500 : duration;
+    easing = isNaN(easing) ? function (t) { return t; } : easing;
+    
+    var clock = 25;
+
+    var count = 0;
+    var start = this.scrollTop();
+
+    if (duration <= 0) {
+        setTimeout(function () {
+            window.scrollTop(0, to);
+        });
+        return;
+    }
+
+    var loop = setInterval(function () {
+        count++;
+        var t = count / (duration / clock);
+
+        if (t >= 1) {
+            window.scrollTo(0, to);
+            clearInterval(loop);
+            return;
+        }
+
+        window.scrollTo(0, start + (to - start) * easing(t));
+    }, clock);
+};
+
+ScrollTracker.prototype.jumpToSection = function (sectionSelector, duration, easing) {
+    var el = null;
+    if (!isNaN(sectionSelector)) {
+        el = this.getSectionElementByIndex(sectionSelector);
+    } else {
+        el = this.getSectionElementById(sectionSelector);
+    }
+
+    if (!el) {
+        return null;
+    }
+
+    return this.jumpTo(el.offsetTop, duration, easing);
+};
+
+ScrollTracker.prototype.getSectionElementById = function (id) {
+    var $section = this.$section;
+    if (!$section) {
+        return null;
+    }
+
+    return $section.filter('#' + id).get(0);
+};
+
+ScrollTracker.prototype.getSectionElementByIndex = function (index) {
+    var $section = this.$section;
+    if (!$section) {
+        return null;
+    }
+
+    return $section.get(index);
 };
